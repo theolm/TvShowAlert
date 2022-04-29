bbe701a66bbfeefcad82558fc3a6ea86925f0fbd
DESKTOP-BANAR6E\Theo <theolm.mota@gmail.com>
2022-04-29 15:21:22 -0300

    All should pass

--- app/src/androidTest/java/com/theolm/tvshowalert/ui/components/MainAppBarKtTest.kt
+++ app/src/androidTest/java/com/theolm/tvshowalert/ui/components/MainAppBarKtTest.kt
@@ -28,6 +28,7 @@ class MainAppBarKtTest {
     @Test
     fun testText() {
         getComponent().onChild().assertTextEquals("Tv Shows")
+        //All should pass
     }
 
     private fun getComponent() = composeRule.onNodeWithTag(MainAppBar.TestTag, useUnmergedTree = true)
