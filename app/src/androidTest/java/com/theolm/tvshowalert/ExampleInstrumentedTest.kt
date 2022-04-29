4ed13e0a78b849c4d9a6d550bd335f4c2113d9ab
DESKTOP-BANAR6E\Theo <theolm.mota@gmail.com>
2022-04-29 18:52:06 -0300

    fix lint

--- app/src/androidTest/java/com/theolm/tvshowalert/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/theolm/tvshowalert/ExampleInstrumentedTest.kt
@@ -2,13 +2,9 @@ package com.theolm.tvshowalert
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-//import org.junit.Assert.assertEquals
-//import org.junit.Test
-//import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
