fc01131907f15d8f511bc39240ae2f8e0af73e95
DESKTOP-BANAR6E\Theo <theolm.mota@gmail.com>
2022-04-29 18:36:58 -0300

    Ktlint should fail

--- app/src/androidTest/java/com/theolm/tvshowalert/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/theolm/tvshowalert/ExampleInstrumentedTest.kt
@@ -6,6 +6,10 @@ import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 
+//import org.junit.Assert.assertEquals
+//import org.junit.Test
+//import org.junit.runner.RunWith
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
--- app/src/test/java/com/theolm/tvshowalert/ExampleUnitTest.kt
+++ app/src/test/java/com/theolm/tvshowalert/ExampleUnitTest.kt
@@ -1,6 +1,7 @@
 package com.theolm.tvshowalert
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 /**
